d3b1545771ff6f18d35bc8b06fcee689ad8b1054
bugbiteme <leon.s.levy@gmail.com>
2021-03-08 07:54:57 -0800

    IPV6 error

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -10,7 +10,7 @@ module "vpc" {
   azs                              = data.aws_availability_zones.available.names
   private_subnets                  = ["10.0.1.0/24", "10.0.2.0/24"]
   public_subnets                   = ["10.0.101.0/24", "10.0.102.0/24"]
-  assign_generated_ipv6_cidr_block = true
+  #assign_generated_ipv6_cidr_block = true
   create_database_subnet_group     = true
   enable_nat_gateway               = true
   single_nat_gateway               = true
